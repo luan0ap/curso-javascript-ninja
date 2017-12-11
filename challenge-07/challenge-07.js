4596bc5f1fad8ec228523cd893a392a3afa0b5ba
Luan AP <luanap.kun@gmail.com>
2017-12-11 15:29:59 -0200

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -9,10 +9,8 @@ Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 
-function addItem() {
-  array.push('hatuna matata');
-  array.push(12345);
-  array.push( [ 1, 'hello' ] )
+function addItem( item ) {
+  array.push( item );
   
   return array
 }
@@ -22,8 +20,7 @@ Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 
-array.push( [ 1, 'string', String ] )
-console.log( array )
+console.log( addItem( [ 1, 'string', String ] ) )
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
@@ -31,7 +28,7 @@ frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
 
-console.log("O segundo elemento do segundo array é " + array[8][1] + " .")
+console.log("O segundo elemento do segundo array é " + array[5][1] + " .")
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
@@ -45,7 +42,7 @@ Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
 
-console.log( "O segundo array tem " array[8].length + " itens." ) 
+console.log( "O segundo array tem " array[5].length + " itens." ) 
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
