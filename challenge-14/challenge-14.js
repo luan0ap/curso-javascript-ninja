3aa97e2502fd73e748b3d3a9f6954a058bf6eba8
Luan AP <luanap.kun@gmail.com>
2018-01-10 14:27:15 -0200

    Update challenge-14.js

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -10,6 +10,18 @@ Os números devem ser de 1 a 10.
 Mostre esse array no console.
 */
 console.log( 'Number Objects Array:' );
+var numberObjects = [
+  { number: 1 },
+  { number: 2 },
+  { number: 3 },
+  { number: 4 },
+  { number: 5 },
+  { number: 6 },
+  { number: 7 },
+  { number: 8 },
+  { number: 9 },
+  { number: 10 }
+]
 // ?
 
 /*
@@ -17,7 +29,9 @@ Crie um array chamado `justNumbers`, que terá como elementos somente os
 números do array criado acima. Mostre esse novo array no console.
 */
 console.log( '\nJust Numbers:' );
-// ?
+var justNumbers = numberObjects.map( function (obj) {
+  return obj.number
+})
 
 /*
 Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
@@ -25,7 +39,9 @@ somente os números que forem divisíveis por 2 ou 3. Mostre esse novo array
 no console.
 */
 console.log( '\nJust module of division by 2 or 3:' );
-// ?
+var justMod2Or3 = justNumbers.filter( function (num) {
+  return num % 2 === 0 || num % 3 === 0
+})
 
 /*
 Declare uma variável chamada operation que receba, do array criado acima,
@@ -36,14 +52,20 @@ O cálculo deve começar com zero.
 Mostre o resultado no console.
 */
 console.log( '\nOperation:' );
-// ?
-
+var operation = justMod2Or3.reduce( function(acc, cur) {
+  return (acc + 1) * cur
+}, 0)
+console.log(operation)
 /*
 Faça o mesmo cálculo passado acima, mas começando do último item para o
 primeiro. O nome da variável deve ser operation2. Mostre o resultado no
 console.
 */
 console.log( '\nOperation 2:' );
+var operation2 = justMod2Or3.reduceRigth( function(acc, cur) {
+  return (acc + 1) * cur
+}, 0)
+console.log(operation)
 // ?
 
 /*
@@ -55,6 +77,10 @@ infantil, onde você coloca a letra "P" antes de cada sílaba de uma palavra
 falada, como se você estivesse falando em código xD
 */
 console.log( '\nSeu nome na língua do "P":' );
+var name = [ 'l', 'u', 'a', 'n' ]
+name.reduce( function ( acc, cur ) {
+  return acc + (cur + 'p')
+}, '')
 // ?
 
 /*
@@ -62,13 +88,13 @@ Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
 e atribuirá o seu nome invertido (usando o array criado acima).
 */
 console.log( '\nInversed Name:' );
-// ?
+var inversedName = name.reverse().join( '' )
 
 /*
 Mostre no console o array `numberObjects`.
 */
 console.log( '\nNumber objects' );
-// ?
+console.log(numberObjects)
 
 /*
 Verifique se existem em algum índice de numberObjects um objeto ìgual a
@@ -80,18 +106,33 @@ Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
 o que acontece ;)
 */
 console.log( '\nExiste um { number: 2 } em numberObjects?' );
-// ?
+function exists (arr, fil) {
+  if (arr.indexOf(fil) !== -1 ) {
+    return 'Existe um objeto { number: 2 } em numberObjects!'
+  }
+  
+  return 'Não existe um objeto { number: 2 } em numberObjects :('
+}
 
+console.log(exists (numberObjects ,'{ number: 2 }'))
 /*
 Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
 será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
 */
 console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-// ?
+function existsReverse (arr, fil) {
+  if (arr.lastIndexOf(fil, 2) !== -1) {
+    return 'Existe um objeto { number: 2 } em numberObjects!'
+  }
+  
+  return 'Não existe um objeto { number: 2 } em numberObjects :('
+}
+
+console.log(existsReverse (numberObjects ,'{ number: 2 }'))
 
 /*
 Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
 formato de String.
 */
 console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-// ?
+console.log(Array.isArray(justMod2Or3) ? justMod2Or3.join('') : '')
